0c493b02b34572487a6b8d4cc598afce698bd1cf
Sean LI <lixiang02thu@gmail.com>
2014-01-20 22:29:54 +0800

    Initial Commit

---
+++ BBRingSelector/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BBRingSelector
+//
+//  Created by Xiang LI on 14-1-20.
+//  Copyright (c) 2014年 bigbelldev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BBRingSelector/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BBRingSelector
+//
+//  Created by Xiang LI on 14-1-20.
+//  Copyright (c) 2014年 bigbelldev.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
